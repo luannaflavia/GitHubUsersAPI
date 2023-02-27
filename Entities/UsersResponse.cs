63fbc979dd95d0d2be7bed2645a46c50e756cc44
Luanna Flavia <luaannaflavia._@live.com>
2023-02-27 20:07:38 -0300

    Novo

---
+++ Entities/UsersResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GitHubUsersAPI.Entities
+{
+    public class UsersResponse
+    {
+        public List<Users> users { get; set; }  
+        public string nextPage { get; set; }   
+        
+    }
+}
---
+++ Services/UserDetailsService.cs
@@ -0,0 +1,20 @@
+﻿using GitHubUsersAPI.Interfaces;
+using RestSharp;
+using System.Threading.Tasks;
+
+namespace GitHubUsersAPI.Service
+{
+    public class UserDetailsService : IUserDetails
+    {
+        public async Task<RestResponse> GetUserDetails(string username)
+        {
+            var client = new RestClient("https://api.github.com/");
+            var request = new RestRequest($"users/{username}", Method.Get);
+            request.AddHeader("Authorization", "Bearer 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");
+            var response = await client.ExecuteGetAsync(request);
+
+            return response;
+        }
+
+    }
+}
